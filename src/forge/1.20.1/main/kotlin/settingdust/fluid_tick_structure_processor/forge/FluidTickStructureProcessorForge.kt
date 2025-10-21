35d840466e667bfe2e8343ffbaf38b4b4e3604f2
SettingDust <settingdust@gmail.com>
2025-10-21 23:26:27 +0800

    fix: register processor on forge

--- src/forge/1.20.1/main/kotlin/settingdust/fluid_tick_structure_processor/forge/FluidTickStructureProcessorForge.kt
+++ src/forge/1.20.1/main/kotlin/settingdust/fluid_tick_structure_processor/forge/FluidTickStructureProcessorForge.kt
@@ -1,10 +1,14 @@
 package settingdust.fluid_tick_structure_processor.forge
 
+import net.minecraft.core.registries.Registries
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.registries.RegisterEvent
 import settingdust.fluid_tick_structure_processor.FluidTickStructureProcessor
+import settingdust.fluid_tick_structure_processor.FluidTickStructureProcessorTypes
 import settingdust.fluid_tick_structure_processor.util.Entrypoint
+import settingdust.fluid_tick_structure_processor.util.StructureProcessorTypeAdapter
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 @Mod(FluidTickStructureProcessor.ID)
@@ -17,6 +21,16 @@ object FluidTickStructureProcessorForge {
                 Entrypoint.init()
             }
             addListener<FMLClientSetupEvent> { Entrypoint.clientInit() }
+            addListener<RegisterEvent> { event ->
+                when (event.registryKey) {
+                    Registries.STRUCTURE_PROCESSOR -> FluidTickStructureProcessorTypes.register { id, value ->
+                        event.register(
+                            Registries.STRUCTURE_PROCESSOR,
+                            id
+                        ) { StructureProcessorTypeAdapter.createFromMapCodec(value) }
+                    }
+                }
+            }
         }
     }
 }
--- src/neoforge/1.21/main/kotlin/settingdust/fluid_tick_structure_processor/neoforge/FluidTickStructureProcessorNeoForge.kt
+++ src/neoforge/1.21/main/kotlin/settingdust/fluid_tick_structure_processor/neoforge/FluidTickStructureProcessorNeoForge.kt
@@ -1,10 +1,14 @@
 package settingdust.fluid_tick_structure_processor.neoforge
 
+import net.minecraft.core.registries.Registries
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
+import net.neoforged.neoforge.registries.RegisterEvent
 import settingdust.fluid_tick_structure_processor.FluidTickStructureProcessor
+import settingdust.fluid_tick_structure_processor.FluidTickStructureProcessorTypes
 import settingdust.fluid_tick_structure_processor.util.Entrypoint
+import settingdust.fluid_tick_structure_processor.util.StructureProcessorTypeAdapter
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
 @Mod(FluidTickStructureProcessor.ID)
@@ -17,6 +21,16 @@ object FluidTickStructureProcessorNeoForge {
                 Entrypoint.init()
             }
             addListener<FMLClientSetupEvent> { Entrypoint.clientInit() }
+            addListener<RegisterEvent> { event ->
+                when (event.registryKey) {
+                    Registries.STRUCTURE_PROCESSOR -> FluidTickStructureProcessorTypes.register { id, value ->
+                        event.register(
+                            Registries.STRUCTURE_PROCESSOR,
+                            id
+                        ) { StructureProcessorTypeAdapter.createFromMapCodec(value) }
+                    }
+                }
+            }
         }
     }
 }
